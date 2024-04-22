85e633eba1a42f17b17afe1ba5dfb0f9162d8a05
oladeji anthony <oladejianthony98@gmail.com>
2024-04-22 23:51:21 +0100

    food page components

--- app/screens/FoodPage.jsx
+++ app/screens/FoodPage.jsx
@@ -1,8 +1,10 @@
 import {
   FlatList,
   Image,
+  ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -10,6 +12,7 @@ import React, { useContext, useState } from "react";
 import { CartCountContext } from "../context/CartCountContext";
 import { COLORS, SIZES } from "../constants/theme";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import BouncyCheckBox from "react-native-bouncy-checkbox";
 
 const FoodPage = ({ route, navigation }) => {
   //we received the "route" from FoodNavigator component and the "navigation" from NewFoodList component
@@ -26,96 +29,132 @@ const FoodPage = ({ route, navigation }) => {
 
   return (
     <View style={{ backgroundColor: COLORS.lightWhite, height: SIZES.height }}>
-      <View>
-        <Image
-          source={{ uri: item.imageUrl[0] }}
-          style={{
-            width: SIZES.width,
-            height: SIZES.height / 4,
-            borderBottomRightRadius: 30,
-          }}
-        />
-
-        <TouchableOpacity
-          onPress={() => navigation.goback()}
-          style={styles.backbtn}
-        >
-          <Ionicons
-            name="chevron-back-circle"
-            size={30}
-            color={COLORS.primary}
+      <ScrollView>
+        <View>
+          <Image
+            source={{ uri: item.imageUrl[0] }}
+            style={{
+              width: SIZES.width,
+              height: SIZES.height / 4,
+              borderBottomRightRadius: 30,
+            }}
           />
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => {}} style={styles.sharebtn}>
-          <MaterialCommunityIcons
-            name="share-circle"
-            size={30}
-            color={COLORS.primary}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.goback()}
+            style={styles.backbtn}
+          >
+            <Ionicons
+              name="chevron-back-circle"
+              size={30}
+              color={COLORS.primary}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => {}}
-          style={{ position: "absolute", bottom: 20, right: 0 }}
-        >
-          <View style={styles.restBtn}>
-            <Text
-              style={{
-                color: COLORS.lightWhite,
-                fontFamily: "bold",
-              }}
-            >
-              Open the Store
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity onPress={() => {}} style={styles.sharebtn}>
+            <MaterialCommunityIcons
+              name="share-circle"
+              size={30}
+              color={COLORS.primary}
+            />
+          </TouchableOpacity>
 
-      <View style={styles.container}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <Text style={styles.title}>{item.title}</Text>
-          <Text style={[styles.title, { color: COLORS.primary }]}>
-            ${(item.price + totalPrice) * count}
-          </Text>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{ position: "absolute", bottom: 20, right: 0 }}
+          >
+            <View style={styles.restBtn}>
+              <Text
+                style={{
+                  color: COLORS.lightWhite,
+                  fontFamily: "bold",
+                }}
+              >
+                Open the Store
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
 
-        <Text style={styles.small}>{item.description}</Text>
-        <FlatList
-          data={item.foodTags}
-          showsVerticalScrollIndicator={false}
-          horizontal
-          scrollEnabled
-          style={{ marginTop: 10 }}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <View style={styles.tags}>
-              <Text style={{ paddingHorizontal: 4, color: COLORS.lightWhite }}>
-                {item}
-              </Text>
-            </View>
-          )}
-        />
+        <View style={styles.container}>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={[styles.title, { color: COLORS.primary }]}>
+              ${(item.price + totalPrice) * count}
+            </Text>
+          </View>
 
-        <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
-          Additives and Toppings
-        </Text>
-        <FlatList
-          data={item.foodTags}
-          showsVerticalScrollIndicator={false}
-          horizontal
-          scrollEnabled
-          style={{ marginTop: 10 }}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <View style={styles.tags}>
-              <Text style={{ paddingHorizontal: 4, color: COLORS.lightWhite }}>
-                {item}
-              </Text>
-            </View>
-          )}
-        />
-      </View>
+          <Text style={styles.small}>{item.description}</Text>
+          <FlatList
+            data={item.foodTags}
+            showsVerticalScrollIndicator={false}
+            horizontal
+            scrollEnabled
+            style={{ marginTop: 10 }}
+            keyExtractor={(item) => item}
+            renderItem={({ item }) => (
+              <View style={styles.tags}>
+                <Text
+                  style={{ paddingHorizontal: 4, color: COLORS.lightWhite }}
+                >
+                  {item}
+                </Text>
+              </View>
+            )}
+          />
+
+          <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
+            Additives and Toppings
+          </Text>
+          <FlatList
+            data={item.additives}
+            showsHorizontalScrollIndicator={false}
+            scrollEnabled={false}
+            style={{ marginTop: 10 }}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  marginBottom: 10,
+                }}
+              >
+                <BouncyCheckBox
+                  size={20}
+                  unfillColor="ffffff"
+                  fillColor={COLORS.primary}
+                  innerIconStyle={{ borderWidth: 1 }}
+                  textStyle={styles.small}
+                  text={item.title}
+                />
+                <Text style={styles.small}>$ {item.price}</Text>
+              </View>
+            )}
+          />
+
+          <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
+            Preferences
+          </Text>
+          <View style={styles.input}>
+            <TextInput
+              placeholder="Add specific instructions"
+              value={preference}
+              onChangeText={(value) => setPreference(value)}
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={{flex: 1}}
+            />
+          </View>
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 20}}>
+            <Text style={[styles.title, {marginBottom: 10}]}>Quantity</Text>
+            
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -167,6 +206,17 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.primary,
     borderRadius: 8,
   },
+  input: {
+    borderColor: COLORS.primary,
+    borderWidth: 1,
+    backgroundColor: COLORS.offwhite,
+    height: 50,
+    flexDirection: "row",
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    alignItems: "center"
+  },
+
 });
 
 //We successfully passed the item data from a component on the homepage into a stack navigator,then to be used on the screen/page in the navigator that we desire using route.params.
