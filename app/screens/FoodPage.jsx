52bad1971faf3dbf6f12b464b7cf22e21c324ee5
oladeji anthony <oladejianthony98@gmail.com>
2024-04-23 12:26:58 +0100

    new commit

--- app/screens/FoodPage.jsx
+++ app/screens/FoodPage.jsx
@@ -1,6 +1,7 @@
 import {
   FlatList,
   Image,
+  SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
@@ -8,13 +9,14 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState,  } from "react";
 import { CartCountContext } from "../context/CartCountContext";
 import { COLORS, SIZES } from "../constants/theme";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import BouncyCheckBox from "react-native-bouncy-checkbox";
 import Counter from "../components/Counter";
-import {AntDesign} from "@expo/vector-icons"
+import { AntDesign } from "@expo/vector-icons";
+
 
 const FoodPage = ({ route, navigation }) => {
   //we received the "route" from FoodNavigator component and the "navigation" from NewFoodList component
@@ -29,169 +31,240 @@ const FoodPage = ({ route, navigation }) => {
   const [preference, setPreference] = useState("");
   //const [cartCount, setCartCount] = useContext(CartCountContext);
 
+  const handleAdditives = (newAdditive) => {
+    setAdditives((prevAdditives) => {
+      const exists = prevAdditives.some(
+        (additive) => additive.id === newAdditive.id
+      );
+      if(exists) {
+        return prevAdditives.filter((additive) => additive.id !== newAdditive.id)
+      }else {
+        return [...prevAdditives, newAdditive]
+      }
+    })
+  }
+  //console.log(additives)
+
+  useEffect(() => {
+    calculatePrice()
+  }, [additives]);
+
+  //additive price
+  const calculatePrice = () => {
+    const total = additives.reduce((sum, additive) => {
+      return sum + parseFloat(additive.price)
+    },0)
+    //console.log(total)
+    setTotalPrice(total)
+  }
+
+  const handlePress = (item) => {
+    
+  }
+ 
+
   return (
-    <View style={{ backgroundColor: COLORS.lightWhite, height: SIZES.height }}>
-      <ScrollView>
-        <View>
-          <Image
-            source={{ uri: item.imageUrl[0] }}
-            style={{
-              width: SIZES.width,
-              height: SIZES.height / 4,
-              borderBottomRightRadius: 30,
-            }}
-          />
-
-          <TouchableOpacity
-            onPress={() => navigation.goback()}
-            style={styles.backbtn}
-          >
-            <Ionicons
-              name="chevron-back-circle"
-              size={30}
-              color={COLORS.primary}
+    <SafeAreaView style={{marginTop: 22}}>
+      <View
+        style={{ backgroundColor: COLORS.lightWhite, height: SIZES.height }}
+      >
+        <ScrollView>
+          <View>
+            <Image
+              source={{ uri: item.imageUrl[0] }}
+              style={{
+                width: SIZES.width,
+                height: SIZES.height / 4,
+                borderBottomRightRadius: 30,
+              }}
             />
-          </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => {}} style={styles.sharebtn}>
-            <MaterialCommunityIcons
-              name="share-circle"
-              size={30}
-              color={COLORS.primary}
-            />
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.goback()}
+              style={styles.backbtn}
+            >
+              <Ionicons
+                name="chevron-back-circle"
+                size={30}
+                color={COLORS.primary}
+              />
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={() => {}}
-            style={{ position: "absolute", bottom: 20, right: 0 }}
-          >
-            <View style={styles.restBtn}>
-              <Text
-                style={{
-                  color: COLORS.lightWhite,
-                  fontFamily: "bold",
-                }}
-              >
-                Open the Store
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity onPress={() => {}} style={styles.sharebtn}>
+              <MaterialCommunityIcons
+                name="share-circle"
+                size={30}
+                color={COLORS.primary}
+              />
+            </TouchableOpacity>
 
-        <View style={styles.container}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={[styles.title, { color: COLORS.primary }]}>
-              ${(item.price + totalPrice) * count}
-            </Text>
-          </View>
-
-          <Text style={styles.small}>{item.description}</Text>
-          <FlatList
-            data={item.foodTags}
-            showsVerticalScrollIndicator={false}
-            horizontal
-            scrollEnabled
-            style={{ marginTop: 10 }}
-            keyExtractor={(item) => item}
-            renderItem={({ item }) => (
-              <View style={styles.tags}>
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{ position: "absolute", bottom: 20, right: 0 }}
+            >
+              <View style={styles.restBtn}>
                 <Text
-                  style={{ paddingHorizontal: 4, color: COLORS.lightWhite }}
+                  style={{
+                    color: COLORS.lightWhite,
+                    fontFamily: "bold",
+                  }}
                 >
-                  {item}
+                  Open the Store
                 </Text>
               </View>
-            )}
-          />
+            </TouchableOpacity>
+          </View>
 
-          <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
-            Additives and Toppings
-          </Text>
-          <FlatList
-            data={item.additives}
-            showsHorizontalScrollIndicator={false}
-            scrollEnabled={false}
-            style={{ marginTop: 10 }}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  marginBottom: 10,
-                }}
-              >
-                <BouncyCheckBox
-                  size={20}
-                  unfillColor="ffffff"
-                  fillColor={COLORS.primary}
-                  innerIconStyle={{ borderWidth: 1 }}
-                  textStyle={styles.small}
-                  text={item.title}
-                />
-                <Text style={styles.small}>$ {item.price}</Text>
-              </View>
-            )}
-          />
+          <View style={styles.container}>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={[styles.title, { color: COLORS.primary }]}>
+                ${(item.price + totalPrice) * count}
+              </Text>
+            </View>
 
-          <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
-            Preferences
-          </Text>
-          <View style={styles.input}>
-            <TextInput
-              placeholder="Add specific instructions"
-              value={preference}
-              onChangeText={(value) => setPreference(value)}
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={{ flex: 1 }}
+            <Text style={styles.small}>{item.description}</Text>
+            <FlatList
+              data={item.foodTags}
+              showsVerticalScrollIndicator={false}
+              horizontal
+              scrollEnabled
+              style={{ marginTop: 10 }}
+              keyExtractor={(item) => item}
+              renderItem={({ item }) => (
+                <View style={styles.tags}>
+                  <Text
+                    style={{ paddingHorizontal: 4, color: COLORS.lightWhite }}
+                  >
+                    {item}
+                  </Text>
+                </View>
+              )}
             />
-          </View>
 
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              marginTop: 20,
-            }}
-          >
-            <Text style={[styles.title, { marginBottom: 10 }]}>Quantity</Text>
-            <Counter count={count} setCount={setCount} />
-          </View>
-        </View>
-        
-        <View style={{ flex: 1, justifyContent: "flex-end" }}>
-          <View style={styles.suspended}>
-            <View style={styles.cart}>
-              <View style={styles.cartRow}>
-                <TouchableOpacity onPress={()=> {}} style={styles.cartbtn}>
-                  <AntDesign  
-                    name="pluscircleo"
-                    size={24}
-                    color={COLORS.lightWhite}
+            <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
+              Additives and Toppings
+            </Text>
+            <FlatList
+              data={item.additives}
+              showsHorizontalScrollIndicator={false}
+              scrollEnabled={false}
+              style={{ marginTop: 10 }}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    marginBottom: 10,
+                  }}
+                >
+                  <BouncyCheckBox
+                    size={20}
+                    unfillColor="ffffff"
+                    fillColor={COLORS.primary}
+                    innerIconStyle={{ borderWidth: 1 }}
+                    textStyle={styles.small}
+                    text={item.title}
+                    onPress={() => {
+                      handleAdditives(item)
+                    }}
                   />
-                </TouchableOpacity>
+                  <Text style={styles.small}>$ {item.price}</Text>
+                </View>
+              )}
+            />
 
-                <TouchableOpacity onPress={()=> navigation.navigate('order-page')} style={{backgroundColor: COLORS.primary, paddingHorizontal: 70, borderRadius: 30}}>
-                  <Text style={[styles.title, {color: COLORS.lightWhite, marginTop: 4, alignItems: "center" }]}>
-                    Order
-                  </Text>
-                </TouchableOpacity>
+            <Text style={[styles.title, { marginBottom: 10, marginTop: 20 }]}>
+              Preferences
+            </Text>
+            <View style={styles.input}>
+              <TextInput
+                placeholder="Add specific instructions"
+                value={preference}
+                onChangeText={(value) => setPreference(value)}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={{ flex: 1 }}
+              />
+            </View>
 
-                <TouchableOpacity onPress={()=> {}} style={{backgroundColor: COLORS.primary, paddingHorizontal: 10, borderRadius: 30}}>
-                  <Text style={[styles.title, {color: COLORS.lightWhite, marginTop: 4, alignItems: "center" }]}>
-                    {0}
-                  </Text>
-                </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginTop: 20,
+              }}
+            >
+              <Text style={[styles.title, { marginBottom: 10 }]}>Quantity</Text>
+              <Counter count={count} setCount={setCount} />
+            </View>
+          </View>
+
+          <View style={{ flex: 1, justifyContent: "flex-end", marginBottom: 40 }}>
+            <View style={styles.suspended}>
+              <View style={styles.cart}>
+                <View style={styles.cartRow}>
+                  <TouchableOpacity onPress={() => {}} style={styles.cartbtn}>
+                    <AntDesign
+                      name="pluscircleo"
+                      size={24}
+                      color={COLORS.lightWhite}
+                    />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("order-page")}
+                    style={{
+                      backgroundColor: COLORS.primary,
+                      paddingHorizontal: 60,
+                      borderRadius: 30,
+                    }}
+                  >
+                    <Text
+                      style={[
+                        styles.title,
+                        {
+                          color: COLORS.lightWhite,
+                          marginTop: 4,
+                          alignItems: "center",
+                        },
+                      ]}
+                    >
+                      Order
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => {}}
+                    style={{
+                      backgroundColor: COLORS.primary,
+                      paddingHorizontal: 10,
+                      borderRadius: 30,
+                    }}
+                  >
+                    <Text
+                      style={[
+                        styles.title,
+                        {
+                          color: COLORS.lightWhite,
+                          marginTop: 4,
+                          alignItems: "center",
+                        },
+                      ]}
+                    >
+                      {0}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </View>
-        </View>
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -260,11 +333,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cart: {
-    width: SIZES.width-24,
+    width: SIZES.width - 24,
     height: 60,
     justifyContent: "center",
     backgroundColor: COLORS.primary1,
-    borderRadius: 30
+    borderRadius: 30,
   },
   cartRow: {
     flexDirection: "row",
@@ -277,12 +350,10 @@ const styles = StyleSheet.create({
     borderRadius: 99,
     justifyContent: "center",
     backgroundColor: COLORS.primary,
-    alignItems: "center"
-  }
-
+    alignItems: "center",
+  },
 });
 
 //We successfully passed the item data from a component on the homepage into a stack navigator,then to be used on the screen/page in the navigator that we desire using route.params.
-
 
 //3hrs,40mins
